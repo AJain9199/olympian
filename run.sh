9637ff9ad0e3f8bdd0e757000f2b892e034a3748
Aryaman Jain <aryamanjain9199@gmail.com>
2024-09-18 21:45:08 +0530

    Basic setup

---
+++ run.sh
@@ -0,0 +1,2 @@
+python manage.py runserver 3040
+cd static && npx tailwindcss -i .\src\input.css -o .\src\styles.css --watch
---
+++ static/tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+      "../templates/*.html",
+      './node_modules/flowbite/**/*.js'
+  ],
+  theme: {
+    // colors: {
+    //   "drk": '#2A2A2A',
+    //   "blue": '#87CEEB',
+    //   "yellow": '#FFD700',
+    //   "green": '#98FB98',
+    //   "red": '#FFA07A',
+    // },
+    extend: {
+      colors: {
+        "drk": '#2A2A2A',
+        "drk-accent": '#3A3A3A',
+        "blue": '#87CEEB',
+        "yellow": '#FFD700',
+        "green": '#98FB98',
+        "red": '#FFA07A',
+      }
+    },
+  },
+  plugins: [],
+}
+
