6f1b37434773ea824e29157f38ea59826e3240cc
Aryaman Jain <aryamanjain9199@gmail.com>
2024-09-21 22:01:07 +0530

    fixed html problems

--- olympics/views.py
+++ olympics/views.py
@@ -3,12 +3,13 @@ from .models import Athlete
 
 
 def index(request):
-    return render(request, 'base.html')
+    return render(request, 'home.html')
 
 
 def athlete(request, athlete_id):
     data = Athlete.objects.get(id=athlete_id)
     return render(request, 'athlete.html', {'athlete_data': data})
+
 
 def history(request):
     return render(request, 'history.html')
